9d2b9f717fe616a0f3d4561f6bf9d4fe7b642e65
HangNT169 <HangNT169@fpt.edu.vn>
2025-01-06 18:59:17 +0700

    Init project

---
+++ src/main/java/com/poly/sof3022/Sof3022Sd19309Bl1Sp25Application.java
@@ -0,0 +1,13 @@
+package com.poly.sof3022;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sof3022Sd19309Bl1Sp25Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Sof3022Sd19309Bl1Sp25Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/poly/sof3022/Sof3022Sd19309Bl1Sp25ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.poly.sof3022;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sof3022Sd19309Bl1Sp25ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
